ededa706585f71cf70eab94abd696807cebe6bd9
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-08-08 23:37:53 +0200

    fix: updating credentials

--- src/main/services/messaging.ts
+++ src/main/services/messaging.ts
@@ -119,7 +119,7 @@ export const runMessagingService = (appWindow: AppWindow) => {
         },
       });
 
-      appWindow.webContents.send('credentials-insert', item);
+      appWindow.viewManager.settingsView.webContents.send('credentials-insert', item);
     } else {
       await storage.update({
         scope: 'formfill',
@@ -134,7 +134,7 @@ export const runMessagingService = (appWindow: AppWindow) => {
         },
       });
 
-      appWindow.webContents.send('credentials-update', { ...data, hostname });
+      appWindow.viewManager.settingsView.webContents.send('credentials-update', { ...data, hostname });
     }
 
     await setPassword('wexond', `${hostname}-${username}`, password);
@@ -154,7 +154,8 @@ export const runMessagingService = (appWindow: AppWindow) => {
     });
 
     await deletePassword('wexond', `${view.hostname}-${fields.username}`);
-    appWindow.webContents.send('credentials-remove', _id);
+
+    appWindow.viewManager.settingsView.webContents.send('credentials-remove', _id);
   });
 
   ipcMain.on('credentials-get-password', async (e, id: string, account: string) => {
